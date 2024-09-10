c21f516b508ccd5cb40c8824bd4c525ac6ce231d
i.stebl <steblivec1000@gmail.com>
2024-09-10 16:41:13 +0300

    feat: lesson 1, integrate left riemann func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("Let's integrate!")
+function testFunc(x) {
+    return x * x - Math.cos(x);
+}
+
+function integrateLeftRiemann(func, step) {
+    let a = parseInt(prompt('Enter a: '));
+    let b = parseInt(prompt('Enter b: '));
+    let total = 0;
+    for (let x = a; x < b; x += step) {
+        total += func(x) * step;
+    }
+    return total;
+}
+
+
+const step = 0.01; // step
+
+const result = integrateLeftRiemann(testFunc, step);
+console.log(result); 
